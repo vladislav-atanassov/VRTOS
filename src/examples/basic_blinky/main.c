1e05c5bf4aaf3d333c4d5efa3eca55b30ee8ea16
Vladislav Atanassov <vlavalat@gmail.com>
2025-07-27 23:16:00 +0300

    - Fixed inludes - Updated theblink delay to be biiger (more noticable) - Using the ST default SystemClock_Config and MX_GPIO_Init instead my user defined - Using artifitial delay with for loop instead of rtos_delay_ms() because it is still not working correctly - Added debug log -  Added signalization in case of hard fault with my user defined HardFault_Handler

--- src/examples/basic_blinky/main.c
+++ src/examples/basic_blinky/main.c
@@ -5,9 +5,10 @@
  * Date: 2025
  ******************************************************************************/
 
-#include "VRTOS/VRTOS.h"
-#include "VRTOS/task.h"
+#include "VRTOS.h"
+#include "task.h"
 #include "stm32f4xx_hal.h"
+#include "log.h"
 
 /**
  * @file main.c
@@ -29,75 +30,10 @@
 #define BLINK_TASK_STACK_SIZE (256U)
 
 /* Blink timing */
-#define LED_BLINK_DELAY_MS (500U)
+#define LED_BLINK_DELAY_MS (5000U)
 
-/* Function prototypes */
-static void system_clock_config(void);
-static void led_init(void);
-static void led_toggle(void);
-static void blink_task(void *param);
-
-/**
- * @brief Configure system clock to 84MHz
- */
-static void system_clock_config(void) {
-    /* Enable HSE */
-    RCC->CR |= RCC_CR_HSEON;
-    while (!(RCC->CR & RCC_CR_HSERDY))
-        ;
-
-    /* Configure PLL: HSE * (N/M) / P = 8MHz * (168/4) / 2 = 168MHz */
-    /* But we want 84MHz, so: HSE * (168/4) / 4 = 84MHz */
-    RCC->PLLCFGR = (4 << RCC_PLLCFGR_PLLM_Pos) |   /* M = 4 */
-                   (168 << RCC_PLLCFGR_PLLN_Pos) | /* N = 168 */
-                   (1 << RCC_PLLCFGR_PLLP_Pos) |   /* P = 4 (01 = /4) */
-                   RCC_PLLCFGR_PLLSRC_HSE;         /* HSE as source */
-
-    /* Enable PLL */
-    RCC->CR |= RCC_CR_PLLON;
-    while (!(RCC->CR & RCC_CR_PLLRDY))
-        ;
-
-    /* Configure Flash latency for 84MHz */
-    FLASH->ACR = FLASH_ACR_LATENCY_2WS | FLASH_ACR_ICEN | FLASH_ACR_DCEN;
-
-    /* Configure AHB, APB1, APB2 prescalers */
-    RCC->CFGR |= RCC_CFGR_HPRE_DIV1 |  /* AHB = SYSCLK */
-                 RCC_CFGR_PPRE1_DIV2 | /* APB1 = AHB/2 = 42MHz */
-                 RCC_CFGR_PPRE2_DIV1;  /* APB2 = AHB = 84MHz */
-
-    /* Switch to PLL */
-    RCC->CFGR |= RCC_CFGR_SW_PLL;
-    while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL)
-        ;
-
-    /* Update SystemCoreClock variable */
-    SystemCoreClock = 84000000;
-}
-
-/**
- * @brief Initialize LED GPIO using direct register access
- */
-static void led_init(void) {
-    /* Enable GPIOA clock */
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-
-    /* Configure PA5 as output */
-    LED_PORT->MODER &= ~(3U << (LED_PIN * 2)); /* Clear mode bits */
-    LED_PORT->MODER |= (1U << (LED_PIN * 2));  /* Set as output */
-
-    /* Configure as push-pull */
-    LED_PORT->OTYPER &= ~LED_PIN_MASK; /* Push-pull */
-
-    /* Configure speed as low */
-    LED_PORT->OSPEEDR &= ~(3U << (LED_PIN * 2)); /* Low speed */
-
-    /* No pull-up/pull-down */
-    LED_PORT->PUPDR &= ~(3U << (LED_PIN * 2)); /* No pull */
-
-    /* Turn off LED initially */
-    LED_PORT->BSRR = LED_PIN_MASK << 16; /* Reset bit */
-}
+void        SystemClock_Config(void);
+static void MX_GPIO_Init(void);
 
 /**
  * @brief Toggle LED state using direct register access
@@ -119,14 +55,16 @@ static void led_toggle(void) {
  */
 static void blink_task(void *param) {
     (void)param; /* Suppress unused parameter warning */
-
+    log_info("IN blink_task()");
     /* Task main loop */
     while (1) {
         /* Toggle LED */
         led_toggle();
 
         /* Delay for specified time */
-        rtos_delay_ms(LED_BLINK_DELAY_MS);
+        // rtos_delay_ms(LED_BLINK_DELAY_MS);
+        for (volatile int i = 0; i < 5000000; i++)
+            ;
     }
 }
 
@@ -139,11 +77,13 @@ int main(void) {
     rtos_status_t      status;
     rtos_task_handle_t blink_task_handle;
 
-    /* Configure system clock */
-    system_clock_config();
+    /* System initializations */
+    SCB->VTOR = FLASH_BASE; /* Set vector table location */
+    SystemClock_Config();
+    MX_GPIO_Init();
+    __enable_irq(); /* Enable global interrupts */
 
-    /* Initialize LED */
-    led_init();
+    log_uart_init(LOG_LEVEL_ALL);
 
     /* Initialize RTOS */
     status = rtos_init();
@@ -157,8 +97,11 @@ int main(void) {
     }
 
     /* Create blink task */
-    status = rtos_task_create(blink_task, "BLINK", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY,
-                              &blink_task_handle);
+    status =
+        rtos_task_create(blink_task, "BLINK", BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, &blink_task_handle);
+
+    log_info("EXIT CODE OF rtos_task_create(): %d, expected: %d", status, RTOS_SUCCESS);
+
     if (status != RTOS_SUCCESS) {
         /* Task creation failed */
         while (1) {
@@ -167,6 +110,9 @@ int main(void) {
                 ;
         }
     }
+
+    log_info("Blink task func: 0x%08X", (uint32_t)blink_task);
+    log_info("ENTERING rtos_start_scheduler()");
 
     /* Start the RTOS scheduler */
     status = rtos_start_scheduler();
@@ -180,4 +126,80 @@ int main(void) {
     }
 
     return 0;
+}
+
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
+}
+
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+    /* Configure the main internal regulator output voltage */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+
+    /* Initializes the RCC Oscillators according to the specified parameters in the RCC_OscInitTypeDef structure. */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
+
+    /* Initializes the CPU, AHB and APB buses clocks */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+    __HAL_RCC_GPIOH_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+
+    GPIO_InitStruct.Pin = GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+void HardFault_Handler(void) {
+    /* Capture fault registers immediately */
+    uint32_t cfsr = SCB->CFSR;
+    uint32_t hfsr = SCB->HFSR;
+    uint32_t mmfar = SCB->MMFAR;
+    uint32_t bfar = SCB->BFAR;
+    uint32_t psp = __get_PSP();
+    uint32_t msp = __get_MSP();
+
+    /* Direct register access for debugging */
+    log_error("HardFault Registers:");
+    log_error(" CFSR=0x%08X HFSR=0x%08X", cfsr, hfsr);
+    log_error(" MMFAR=0x%08X BFAR=0x%08X", mmfar, bfar);
+    log_error(" PSP=0x%08X MSP=0x%08X", psp, msp);
+
+    /* Infinite loop with LED blink */
+    while (1) {
+        led_toggle();
+        for (volatile int i = 0; i < 100000; i++)
+            ;
+    }
 }
